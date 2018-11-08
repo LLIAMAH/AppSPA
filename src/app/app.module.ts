e9ebb0be2e0f4aeeb3934f2b2180cb1565d99107
Vasily Himchenko <White_Scorpion@inbox.lv>
2018-11-09 00:12:48 +0200

    Removed experimental components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { UserDataComponent } from './user-data/user-data.component';
-
 @NgModule({
-  declarations: [AppComponent, UserDataComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
